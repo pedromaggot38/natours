0c1aed317a9d965165371930e1417a30098360b0
Pedro Sanches <pedromaggot38@gmail.com>
2025-03-28 18:09:51 -0300

    refactor: refactoring all routes, controllers and server

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    messa: 'Route not defined!',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    messa: 'Route not defined!',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    messa: 'Route not defined!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    messa: 'Route not defined!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    messa: 'Route not defined!',
+  });
+};
