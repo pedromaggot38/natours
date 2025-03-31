c4a90c0e2edecc74e7acd1e2698a4c93838eea94
Pedro Sanches <pedromaggot38@gmail.com>
2025-03-31 15:32:21 -0300

    feat: mongodb connection and .env configuration

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(db, {}).then(() => {
+  console.log('Db conecction successful!');
+});
 
 const port = 3000;
 app.listen(port, () => {
