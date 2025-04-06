c2645d7b488087164aa1d47f74bde15e460e654b
Pedro Sanches <pedromaggot38@gmail.com>
2025-04-06 10:05:40 -0300

    feat: top cheap tours route added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,11 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
+  next();
+};
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
@@ -32,7 +38,7 @@ exports.getAllTours = async (req, res) => {
 
     // Pagination
     const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
     query = query.skip(skip).limit(limit);
