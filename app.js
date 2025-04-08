ae44a3492513d93f6c77fa738a8cf5bce00ceffa
Pedro Sanches <pedromaggot38@gmail.com>
2025-04-08 10:16:25 -0300

    feat: errorController

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
-
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // Middleware -----------------------------
 app.use(express.json());
 app.use((req, res, next) => {
   next();
 });
+
 // ----------------------------------------
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
