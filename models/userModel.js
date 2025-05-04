d007ccd35272939626f1dfb6f336ec37562039c0
Pedro Sanches <pedromaggot38@gmail.com>
2025-05-04 08:13:25 -0300

    feat: deleteMe feature

--- models/userModel.js
+++ models/userModel.js
@@ -37,6 +37,16 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  next();
 });
 
 userSchema.pre('save', async function (next) {
