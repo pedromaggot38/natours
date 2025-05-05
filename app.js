4a6bebdfa5c7d3fdab6eaccedb2751acddc5dfea
Pedro Sanches <pedromaggot38@gmail.com>
2025-05-05 01:01:05 -0300

    feat: request limit implemented

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const app = express();
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-// Middleware -----------------------------
+const app = express();
+
+// Global Middleware ----------------------
 app.use(express.json());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this ip. Please try again in an hour',
+});
+app.use('/api', limiter);
+
 app.use((req, res, next) => {
   next();
 });
