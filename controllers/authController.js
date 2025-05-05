f15ee00e09d4f329a5fd8e3e6f3b17085919e7e4
Pedro Sanches <pedromaggot38@gmail.com>
2025-05-05 00:46:33 -0300

    feat: sending jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,20 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove fields from output
+  user.password = undefined;
+  user.active = undefined;
 
   res.status(statusCode).json({
     status: 'success',
@@ -31,6 +45,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
     role: req.body.role,
     passwordChangedAt: req.body.passwordChangedAt,
   });
